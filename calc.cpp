8fcc8eae1ce197798973694512a2bb9e92a2f3b7
sh1lezh <shailesh811cn@gmail.com>
2024-06-01 20:30:43 +0530

    added files bruh

--- calc.cpp
+++ calc.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cmath>
+#include <string>
+#include <unordered_map>
 using namespace std;
 
 class calculator
@@ -6,6 +9,16 @@ class calculator
 
     int num1, num2, menuInt;
     char op;
+    string trigo;
+    double num_1, num_2;
+
+    enum class trigoFunc
+    {
+        SIN,
+        COS,
+        TAN,
+        UNKNOWN_FUNC
+    };
 
 public:
     void startMenu()
@@ -21,20 +34,26 @@ public:
         cin >> menuInt;
         cout << endl;
 
-        if (menuInt == 1)
+        do
         {
-            while (true)
+            switch (menuInt)
             {
-                normalCalcMenu();
+            case 1:
+                while (true)
+                {
+                    normalCalcMenu();
+                }
+                break;
+            case 2:
+                while (true)
+                {
+                    scientificCalcMenu();
+                }
+                break;
+            default:
+                cout << "Wrong value, enter again." << endl;
             }
-        }
-        else if (menuInt == 2)
-        {
-            while (true)
-            {
-                scientificCalcMenu();
-            }
-        }
+        } while (menuInt == 1 || menuInt == 2);
     }
 
     void normalCalcMenu()
@@ -43,6 +62,7 @@ public:
         cout << "Normal Calculator !" << endl
              << "Enter your operation (+ - * /) : ";
         cin >> num1 >> op >> num2;
+        cout<<endl;
         normalCalc(num1, op, num2);
     }
 
@@ -65,17 +85,118 @@ public:
             break;
         default:
             cout << "Wrong value, enter again!" << endl;
+            break;
         }
-        cout << "Your answer is " << ans << endl
+        cout << "--------------------" << endl
+             << "| Your answer is " << ans << " |" << endl
+             << "--------------------" << endl
              << endl;
     }
 
     void scientificCalcMenu()
     {
+        cout << "------------------------" << endl
+             << "Scientific Calculator !" << endl
+             << "1. Trigonometric Function" << endl
+             << "2. Exp and Log Function" << endl
+             << "3. Power Function" << endl
+             << "Enter your operation number : ";
+        cin >> menuInt;
+        cout << endl;
+        do
+        {
+            switch (menuInt)
+            {
+            case 1:
+                while (true)
+                {
+                    scientificTrigoCalc();
+                }
+                break;
+            case 2:
+                while (true)
+                {
+                    scientificExpLogCalc();
+                }
+                break;
+            case 3:
+                while (true)
+                {
+                    scientificPowerCalc();
+                }
+            default:
+                cout << "Wrong value, enter again." << endl;
+                break;
+            }
+        } while (menuInt == 1 || menuInt == 2 || menuInt == 3);
     }
 
-    void scientificCalc()
+    void scientificExpLogCalc()
     {
+    }
+
+    void scientificPowerCalc()
+    {
+    }
+
+    void scientificTrigoCalc()
+    {
+        cout << "----------------------" << endl;
+        cout << "Trignometric Function" << endl
+             << "1. Sin" << endl
+             << "2. Cos" << endl
+             << "3. Tan" << endl
+             << "Write your trigonometric function (eg., sin 30) : ";
+        cin >> trigo >> num_1;
+        cout << endl;
+
+        num_2 = degreesToRadians(num_1);
+        trigoFunc FUNC = stringToFunc(trigo);
+        switch (FUNC)
+        {
+        case trigoFunc::SIN:
+            cout << "------------------------" << endl;
+            cout << "| Answer of sin " << num_1 << " is " << sin(num_2) << " |" << endl;
+            cout << "------------------------" << endl;
+            break;
+        case trigoFunc::COS:
+            cout << "------------------------" << endl;
+            cout << "| Answer of cos " << num_1 << " is " << cos(num_2) << " |" << endl;
+            cout << "------------------------" << endl;
+            break;
+        case trigoFunc::TAN:
+            cout << "------------------------" << endl;
+            cout << "| Answer of tan " << num_1 << " is " << tan(num_2) << " |" << endl;
+            cout << "------------------------" << endl;
+            break;
+        default:
+            cout << "Wrong value, enter again!" << endl;
+            break;
+        }
+        cout << endl;
+    }
+
+    trigoFunc stringToFunc(const string &trigo)
+    {
+        static const unordered_map<string, trigoFunc> colorMap = {
+            {"sin", trigoFunc::SIN},
+            {"cos", trigoFunc::COS},
+            {"tan", trigoFunc::TAN}};
+
+        auto it = colorMap.find(trigo);
+        if (it != colorMap.end())
+        {
+            return it->second;
+        }
+        else
+        {
+            return trigoFunc::UNKNOWN_FUNC;
+        }
+    };
+
+    double degreesToRadians(double degrees)
+    {
+        return degrees * M_PI / 180.0;
     }
 };
 
